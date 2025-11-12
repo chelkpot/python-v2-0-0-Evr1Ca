e51bb80639b1e913995c984b5c19f70697f5fc6c
Evr1Ca <kalikusarka@gmail.com>
2025-11-12 10:10:29 +0500

    Update task2.py

--- tasks/task2.py
+++ tasks/task2.py
@@ -2,10 +2,12 @@
 
 def solve():
 # Ниже пишите решение задачи
-
-   
-
-   
+    X, Y, Z = map(int, input().split())
+    C_k = 3
+    C_r = C_k + 2
+    C_f = C_r + 7
+    total_cost = X * C_k + Y * C_r + Z * C_f
+    print(total_cost)
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
     solve()
