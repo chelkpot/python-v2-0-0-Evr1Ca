69711794af0d100735a9a1ebb901b0c96aff7cf8
Evr1Ca <kalikusarka@gmail.com>
2025-11-12 10:11:29 +0500

    Update task3.py

--- tasks/task3.py
+++ tasks/task3.py
@@ -2,9 +2,10 @@
 
 def solve():
 # Ниже пишите решение задачи
-    
-
-
+    A, B = map(int, input().split())
+    apples_A = A - B
+    apples_B = B
+    print(apples_A, apples_B    
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
     solve()
