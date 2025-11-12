907e5e8ee55375d4e0b5e15864b98aaa396df290
Evr1Ca <kalikusarka@gmail.com>
2025-11-12 10:09:34 +0500

    Update task1.py

--- tasks/task1.py
+++ tasks/task1.py
@@ -2,17 +2,11 @@
 
 def solve():
 # Ниже пишите решение задачи
-    def main():
     S = int(input())
     x = S // 6
     katya = 4 * x
     print(x, katya, x)
 
-if __name__ == "__main__":
-    main()
-
-
-    
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
     solve()
