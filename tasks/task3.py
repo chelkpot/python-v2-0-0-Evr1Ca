28ff06e9be1d902cda6ad56670494dd292de7545
Evr1Ca <kalikusarka@gmail.com>
2025-11-13 14:21:10 +0500

    Update task3.py

--- tasks/task3.py
+++ tasks/task3.py
@@ -5,7 +5,7 @@ def solve():
     A, B = map(int, input().split())
     apples_A = A - B
     apples_B = B
-    print(apples_A, apples_B    
+    print(apples_A, apples_B)
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
     solve()
